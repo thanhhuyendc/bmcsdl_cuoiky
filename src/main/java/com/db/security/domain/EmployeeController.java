5b249cb813ef575395e2d292b6772b6866529fe9
huyền Trịnh <00thanhthanhhuyen@gmail.com>
2024-12-19 19:28:16 +0700

    fix login

--- src/main/java/com/db/security/domain/EmployeeController.java
+++ src/main/java/com/db/security/domain/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
 import java.util.Objects;
 
 @Controller
@@ -27,15 +28,26 @@ public class EmployeeController {
     }
 
     @PostMapping("")
-    public String postLogin(Model model,@ModelAttribute EmployeeRequest request, RedirectAttributes redirectAttributes) {
+    public String postLogin(
+            Model model,
+            @ModelAttribute EmployeeRequest request,
+            RedirectAttributes redirectAttributes
+    ) {
         dataSourceContextHolder.setBranchContext(DatasourceType.NGUOI_DUNG);
-        if(Objects.equals(request.getUsername(), "NGUOIDUNG")){
+
+        // Kiểm tra username có thuộc danh sách ['XACTHUC', 'XETDUYET', 'LUUTRU', 'GIAMSAT']
+        if (List.of("XACTHUC", "XETDUYET", "LUUTRU", "GIAMSAT").contains(request.getUsername())) {
+            String employee = employeeService.getEmployee(request);
+            redirectAttributes.addAttribute("username", employee);
+            redirectAttributes.addAttribute("role", dataSourceContextHolder.getBranchContext());
+            if (dataSourceContextHolder.getBranchContext()==  DatasourceType.NGUOI_DUNG) {
+                redirectAttributes.addAttribute("message","No privilege for user");
+                return "redirect:/login";
+            }
+            return "redirect:/user/view-all";
+        } else {
             redirectAttributes.addAttribute("message","No privilege for user");
             return "redirect:/login";
         }
-        String employee = employeeService.getEmployee(request);
-        redirectAttributes.addAttribute("username", employee);
-        redirectAttributes.addAttribute("role", dataSourceContextHolder.getBranchContext());
-        return "redirect:/user/view-all";
     }
 }
